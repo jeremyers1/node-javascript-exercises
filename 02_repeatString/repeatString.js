8317c8ab6b997048dc2110e837836f65daff203a
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-27 15:34:24 -0700

    Complete helloWorld and repeatStrings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let stringReturn = '';
+    if (num < 0) return 'ERROR';
+    for (let i = 0; i < num; i++){
+        stringReturn += string;
+    }
+    return stringReturn;
 };
 
 // Do not edit below this line
