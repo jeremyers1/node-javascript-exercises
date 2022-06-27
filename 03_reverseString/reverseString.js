21ec2ff18b33bd885ba4d5f47ce79ce346032acc
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-27 15:59:03 -0700

    Correct Code for reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let strRet = '';
 
+    for (let i = str.length-1; i >= 0; i--){
+        strRet += str[i];
+    }
+
+    return strRet;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// reverseString("Hello");
