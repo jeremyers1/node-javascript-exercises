79d81221b7215607991de0af1ce3d6650a34f08a
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-27 19:58:39 -0700

    Add functional code for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+return Number((Math.round((((temp - 32) * (5 / 9)) * 10)) / 10).toFixed(1));
+// return Number(((temp - 32) * (5 / 9)).toFixed(1)); // works, but other has better accuracy???
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+return Number((Math.round((((temp * (9 / 5)) + 32) * 10)) / 10).toFixed(1));
+// return Number(((temp * (9 / 5)) + 32).toFixed(1)); // works, but other has better accuracy???
 };
 
 // Do not edit below this line
