9b64581df1352843b57328d6743b946f9f51a0ba
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-27 19:11:04 -0700

    Add working code to sumAll:
    Note: I skipped the negative number text, because I want the function to work with negative numbers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return "ERROR";
+    }
+    let total = 0;
+    if (num2 >= num1) {
+        for (let i = num1; i <= num2; i++) {
+            total += i;
+        }
+    } else {
+        for (let i = num2; i <= num1; i++) {
+            total += i;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
