10ce282a60a1404a13518e878b9e1f1ab7d70730
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-27 22:53:52 -0700

    Add functional code to palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let revStr = '';
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+
+    // Help: https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
+    // first remove punctuation
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    
+    // second remove spaces
+    str = str.replace(/\s+/g, '');
+
+    //then make sure string is in lower case
+    str = str.toLowerCase();
+
+    // then reverse to a new string, check, and return
+    for (let i = str.length-1; i >= 0; i--){
+        revStr += str[i];
+    }
+
+    if (str === revStr) return true;
+    else return false;
 
 };
 
