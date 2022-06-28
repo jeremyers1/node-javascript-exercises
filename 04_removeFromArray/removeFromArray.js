0474683445b537c9a86a82e7e7dc28442b444f65
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-27 18:56:29 -0700

    Add working code for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arg) {
+    return arr.filter(item => !arg.includes(item));
 };
+
+// Note: Rest paramaters form an array in JS, so array methods can be used on rest paramaters
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
