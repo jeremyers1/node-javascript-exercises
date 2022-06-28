572e4c4f7b14ac485a1bd4e08871628bb179bf08
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-27 22:56:43 -0700

    Refactor to chain string methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -5,13 +5,19 @@ const palindromes = function (str) {
 
     // Help: https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
     // first remove punctuation
-    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    /* str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
     
     // second remove spaces
     str = str.replace(/\s+/g, '');
 
     //then make sure string is in lower case
     str = str.toLowerCase();
+    */
+    
+    // Can I chain them?
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+             .replace(/\s+/g, '')
+             .toLowerCase();
 
     // then reverse to a new string, check, and return
     for (let i = str.length-1; i >= 0; i--){
