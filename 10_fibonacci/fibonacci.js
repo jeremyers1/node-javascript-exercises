e39a57636c2f677e2b3d3918dc527c3fe3caf6d0
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-28 07:00:13 -0700

    Add working code to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (typeof num != 'number') Number(num);
+    
+    if (num < 0) return "OOPS";
+    else {
+        let num1 = 0, num2 = 1; 
+        let fib = 0;
+        for (let i = 0; i < num; i++){
+            num1 = num2;
+            num2 = fib;
+            fib = num1 + num2;
+        }
+        return fib;
+    }
 };
+
+/* The recursive approach works also, but time complexity is exponential
+const fibonacci = function (num) {
+  if(num < 2){
+    return num;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+} */
 
 // Do not edit below this line
 module.exports = fibonacci;
