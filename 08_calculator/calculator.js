175d3d94501c6ef978b68320a488e4fa53d015b4
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-27 22:23:13 -0700

    Add functional code to Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2; 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  arr.forEach(element => total += element);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let total = 1; 
+  arr.forEach(element => total *= element);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return Math.pow(num, pow); //TOP probably wanted me to do a loop here
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1; 
+  while (num > 0) {
+    total *= num;
+    num--;
+  }
+  return total;
 };
 
 // Do not edit below this line
